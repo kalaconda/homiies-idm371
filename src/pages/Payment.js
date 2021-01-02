a8fceaa8fcb5239f01f4a9f719a6784bdfc376b3
mikaylalisiy <mol25@drexel.edu>
2021-01-02 17:15:40 -0500

    📝 update log file

---
+++ src/pages/Payment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo60x60 from '../images/icon60x60.png';
+
+const payment = () => {
+    return (
+      <div className="App">
+      <div className="white">
+        <div className="topnav">
+          <img src={logo60x60} className="App-logo2" alt="logo" />
+        </div>
+      </div>
+    </div>
+    );
+}
+ 
+export default payment;
