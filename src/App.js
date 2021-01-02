f95f3a19cf00a77dff4d88039aed0c4b9f453e28
mikaylalisiy <mol25@drexel.edu>
2021-01-01 21:52:27 -0500

    🚧 Navigation
    ** look at logfile.txt for details **

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-
+import Welcome from './components/Welcome';
 import Home from './components/Home';
 import Tasks from './components/Tasks';
 import Payment from './components/Payment';
+import Notifications from './components/Notifications';
 import Error from './components/Error';
 import Navigation from './components/Navigation';
  
@@ -16,9 +17,11 @@ class App extends Component {
         <div>
           <Navigation />
             <Switch>
-             <Route path="/" component={Home} exact/>
+            <Route path="/welcome" component={Welcome}/>
+             <Route path="/home" component={Home}/>
              <Route path="/tasks" component={Tasks}/>
              <Route path="/payment" component={Payment}/>
+             <Route path="/notifications" component={Notifications}/>
             <Route component={Error}/>
            </Switch>
         </div> 
