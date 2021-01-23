6c1253a3968e65d3d7b431ecf5e6df6754a7a4d3
mikaylalisiy <mol25@drexel.edu>
2021-01-22 22:41:53 -0500

    🚧 Todo List

--- src/pages/SidebarData.js
+++ src/pages/SidebarData.js
@@ -1,30 +1,25 @@
 import React from "react";
-import * as MdIcons from "react-icons/md";
 
 /* your bottom navigation */
 export const SidebarData = [
   {
     title: "Home",
     path: "/home",
-    icon: <MdIcons.MdDescription />,
     cName: "nav-text"
   },
   {
     title: "Tasks",
     path: "/tasks",
-    icon: <MdIcons.MdDescription />,
     cName: "nav-text"
   },
   {
     title: "Payment",
     path: "/payment",
-    icon: <MdIcons.MdDescription />,
     cName: "nav-text"
   },
   {
     title: "Notifications",
     path: "/notifications",
-    icon: <MdIcons.MdDescription />,
     cName: "nav-text"
   }
 ];
---
+++ src/pages/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoItem = () => {
+    return(
+        <div className="todoItem">
+            <p>hello</p>
+            <span>X</span>
+        </div>
+    )
+}
+
+export default TodoItem;
